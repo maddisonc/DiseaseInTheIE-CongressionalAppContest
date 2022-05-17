46f5462a72ff535b2d7c62ede6fb27bf19a25139
91093567 <91093567@mvusd.net>
2022-05-17 08:46:11 -0700

    Created all screens, dragged in basic elements

---
+++ app/src/main/java/com/example/covidtrackerv1/ChooseSymptoms.java
@@ -0,0 +1,14 @@
+package com.example.covidtrackerv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseSymptoms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_symptoms);
+    }
+}
---
+++ app/src/main/java/com/example/covidtrackerv1/MainMenu.java
@@ -0,0 +1,19 @@
+package com.example.covidtrackerv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    // https://www.youtube.com/watch?v=gR-8-Jhq57E (slideshow)
+
+
+
+}
