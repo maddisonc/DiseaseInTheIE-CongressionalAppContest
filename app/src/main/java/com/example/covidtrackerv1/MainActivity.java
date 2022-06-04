92ba5a9d76a48ebe35f80463ef8c77cc67f33417
Maddison <mtranchau1@gmail.com>
2022-06-04 10:22:48 -0700

    6/4/22 (10:21 am): Splash screen is now functional, removed action bar from all screens for aesthetics (by removing bar from themes).  Next steps: gather stock images for slideshows and make them functional.

--- app/src/main/java/com/example/covidtrackerv1/MainActivity.java
+++ app/src/main/java/com/example/covidtrackerv1/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.covidtrackerv1;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    // code for splash screen
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // runnable that delays switching to next screen
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MainMenu.class);
+                startActivity((i));
+                finish();
+            }
+        }, 5000);
     }
 }
